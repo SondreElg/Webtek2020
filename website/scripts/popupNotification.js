1ffdd8500e65be400db40e1868b97010e273212c
sondrehelgaaen@gmail.com <sondrehelgaaen@gmail.com>
2020-10-15 15:54:28 +0200

    Add screenings.html, clean generatePopup

--- website/scripts/popupNotification.js
+++ website/scripts/popupNotification.js
@@ -1,4 +1,5 @@
-function generatePopup(title, description, imgsrc) {
+function generatePopup_(title, synopsis, imgsrc) {
+    document.getElementById("popup").style.display = "block";
     const html = `
     <div id="popupContent">
         <div id="popupHeader">
@@ -7,7 +8,7 @@ function generatePopup(title, description, imgsrc) {
         <div id="popupBody">
             <img id="popupThumbnail" src="${imgsrc}" alt="${title}" />
             <p id="popupDescription">
-                ${description}
+                ${synopsis}
             </p>
         </div>
         <div id="popupX" onClick="closePopup()"><span>X</span></div>
@@ -20,18 +21,16 @@ function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
 
-generatePopup(
-    "Kaguya-Sama: Love is War",
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Fusce eu quam orci. Praesent eget neque nec neque vehicula
-eleifend et ut neque. Praesent elementum nibh vitae odio
-pulvinar ullamcorper. In eu sapien arcu. Integer pharetra
-justo sed risus elementum, porttitor sagittis ante porta.
-Aliquam faucibus nunc quis neque semper, et pharetra tortor
-vestibulum. Morbi ac ullamcorper nisi. Donec vel congue
-urna, sit amet aliquam orci. Lorem ipsum dolor sit amet,
-consectetur adipiscing elit. Proin lectus sem, luctus eu ex
-eu, volutpat posuere quam. Nullam pellentesque feugiat magna
-sit amet luctus. Morbi pharetra quis massa id interdum.`,
-    "images/anime1.jpg"
-);
+/**
+ * Generates a popup for the provided anime
+ * 
+ * @param {string} anime - the anime for which you're generating a popup
+ */
+function generatePopup(anime) {
+    generatePopup_(
+        anime_dictionary[anime][animeTitle], anime_dictionary[anime][animeSynopsis], anime_dictionary[anime][animeImgsrc]
+    );
+}
+
+//generatePopup("Kaguya");
+
