3214f70c8b977bf419e8cd9225d6a4e7dca6c615
sondrehelgaaen@gmail.com <sondrehelgaaen@gmail.com>
2020-11-02 23:32:33 +0100

    Newsfeed load on scroll

--- website/scripts/showNews.js
+++ website/scripts/showNews.js
@@ -1,33 +1,35 @@
 // Clean this script later, shouldn't need all the logic in showScreenings
 // Alternatively, find a way to separate what
-var newsCount = 0;
+var newsCount = 0
+var loaded = false
 
-function displayFirstTwentyNews() {
-    newsCount += 20;
-    displayNews(0, newsCount);
+function displayFirstTwentyNews () {
+    newsCount += 20
+    displayNews (0, newsCount)
 }
 
-function displayAllNews() {
-    displayNews(newsCount, newsList.length);
-    document.getElementById("buttonDisplayAllNews").style.display = "none";
+function displayAllNews () {
+    displayNews(newsCount, newsList.length - 1)
 }
 
-function displayNews(start, end) {
-    var thisNewsContent;
-    var thisNewsAuthor;
-    var thisNewsDate;
+function displayNews (start, end) {
+    loaded = false
+    var thisNewsContent
+    var thisNewsAuthor
+    var thisNewsDate
 
-    console.log(newsList);
+    if (start >= end) {
+        return
+    }
+
+    if (end >= newsList.length) {
+        end = newsList.length - 1
+    }
+    
     for (i = start; i < end; i++) {
-        if (i > newsList.length) {
-            return;
-        }
-
-        thisNewsContent = newsList[i][newsContent];
-        thisNewsAuthor = newsList[i][newsAuthor];
-        thisNewsDate = newsList[i][newsDate];
-
-        console.log(newsList[i]);
+        thisNewsContent = newsList[i][newsContent]    
+        thisNewsAuthor = newsList[i][newsAuthor]    
+        thisNewsDate = newsList[i][newsDate]
 
         document.getElementById("newsfeed_container").innerHTML += `
             <div class="news_content">
@@ -35,14 +37,16 @@ function displayNews(start, end) {
                     ${thisNewsContent}
                 </p>
                 <span class="author">-${thisNewsAuthor}, ${thisNewsDate}</span>
-            </div>`;
+            </div>`
     }
+    newsCount = end
+    loaded = true
 }
 
-function displayLatestNews() {
-    const latestContent = newsList[0][newsContent];
-    const latestAuthor = newsList[0][newsAuthor];
-    const latestDate = newsList[0][newsDate];
+function displayLatestNews () {
+    const latestContent = newsList[0][newsContent]    
+    const latestAuthor = newsList[0][newsAuthor]    
+    const latestDate = newsList[0][newsDate]    
 
     document.getElementById("news_section").innerHTML += `
     <div class="news_content">
@@ -50,5 +54,14 @@ function displayLatestNews() {
             ${latestContent}
         </p>
         <span class="author">-${latestAuthor}, ${latestDate}</span>
-    </div>`;
+    </div>`
+}
+
+function checkScroll () {
+    console.log(window.scrollY, document.documentElement.scrollHeight)
+    if (window.scrollY + 1200 >= document.documentElement.scrollHeight && loaded) {
+        loaded = false
+        displayNews(newsCount, newsCount + 20)
+        newsCount += 20;
+    }
 }
