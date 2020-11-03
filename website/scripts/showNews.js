9c7ef5f4e1d49bcc01eb0378ac6819c9583018a1
Aleksander Knutsen <al3xknutsen@gmail.com>
2020-11-03 18:29:45 +0100

    Fixing html validation: part 1

--- website/scripts/showNews.js
+++ website/scripts/showNews.js
@@ -1,67 +1,70 @@
 // Clean this script later, shouldn't need all the logic in showScreenings
 // Alternatively, find a way to separate what
-var newsCount = 0
-var loaded = false
+var newsCount = 0;
+var loaded = false;
 
-function displayFirstTwentyNews () {
-    newsCount += 20
-    displayNews (0, newsCount)
+function displayFirstTwentyNews() {
+    newsCount += 20;
+    displayNews(0, newsCount);
 }
 
-function displayAllNews () {
-    displayNews(newsCount, newsList.length - 1)
+function displayAllNews() {
+    displayNews(newsCount, newsList.length - 1);
 }
 
-function displayNews (start, end) {
-    loaded = false
-    var thisNewsContent
-    var thisNewsAuthor
-    var thisNewsDate
+function displayNews(start, end) {
+    loaded = false;
+    var thisNewsContent;
+    var thisNewsAuthor;
+    var thisNewsDate;
 
     if (start >= end) {
-        return
+        return;
     }
 
     if (end >= newsList.length) {
-        end = newsList.length - 1
+        end = newsList.length - 1;
     }
-    
+
     for (i = start; i < end; i++) {
-        thisNewsContent = newsList[i][newsContent]    
-        thisNewsAuthor = newsList[i][newsAuthor]    
-        thisNewsDate = newsList[i][newsDate]
+        thisNewsContent = newsList[i][newsContent];
+        thisNewsAuthor = newsList[i][newsAuthor];
+        thisNewsDate = newsList[i][newsDate];
 
         document.getElementById("newsfeed_container").innerHTML += `
-            <div class="news_content">
+            <div class="info_content">
                 <p>
                     ${thisNewsContent}
                 </p>
                 <span class="author">-${thisNewsAuthor}, ${thisNewsDate}</span>
-            </div>`
+            </div>`;
     }
-    newsCount = end
-    loaded = true
+    newsCount = end;
+    loaded = true;
 }
 
-function displayLatestNews () {
-    const latestContent = newsList[0][newsContent]    
-    const latestAuthor = newsList[0][newsAuthor]    
-    const latestDate = newsList[0][newsDate]    
+function displayLatestNews() {
+    const latestContent = newsList[0][newsContent];
+    const latestAuthor = newsList[0][newsAuthor];
+    const latestDate = newsList[0][newsDate];
 
     document.getElementById("news_section").innerHTML += `
-    <div class="news_content">
+    <div class="info_content">
         <p>
             ${latestContent}
         </p>
         <span class="author">-${latestAuthor}, ${latestDate}</span>
-    </div>`
+    </div>`;
 }
 
-function checkScroll () {
-    console.log(window.scrollY, document.documentElement.scrollHeight)
-    if (window.scrollY + 1200 >= document.documentElement.scrollHeight && loaded) {
-        loaded = false
-        displayNews(newsCount, newsCount + 20)
+function checkScroll() {
+    console.log(window.scrollY, document.documentElement.scrollHeight);
+    if (
+        window.scrollY + 1200 >= document.documentElement.scrollHeight &&
+        loaded
+    ) {
+        loaded = false;
+        displayNews(newsCount, newsCount + 20);
         newsCount += 20;
     }
 }
